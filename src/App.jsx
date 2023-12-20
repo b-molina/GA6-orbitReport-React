75214cdaa7438c7efd29aa08cadd3ff775b4279d
Bryanna Molina <bryannamolina@bryannas-mbp.myfiosgateway.com>
2023-12-20 12:44:37 -0500

    GA6 babbbbbbyyyy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-
+import Banner from "./components/Banner";
+//task 3.1
+import { useState } from "react";
+//task 3.2
+import satData from "./components/satData";
 
 function App() {
+  //task 3.2
+  const [sat, setSat] = useState(satData);
+  //task 3.4
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+
+  //task 3.5
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+  };
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat} />
     </div>
   );
 }
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,18 @@
+//optional styling
+import "./styling.css";
+//task 4.1
+import satData from "./satData";
+
+//task 4.2 Pass the props created from the App() function: filterByType, setSat, and displaySats.
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className="flex-container">
+      {displaySats.map((sat, id) => (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+      ))}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
